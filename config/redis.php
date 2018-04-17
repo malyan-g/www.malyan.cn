b681e0ef5221dc6571db0f672c7582d103e371ec
maliang <maliang@xywy.com>
2018-04-18 00:17:45 +0800

    all

--- config/redis.php
+++ config/redis.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\redis\Connection',
     'hostname' => $SYSTEM_CONFIG['SYSTEM_REDIS_HOST'],
     'port' => $SYSTEM_CONFIG['SYSTEM_REDIS_PORT'],
-    //'password' => $SYSTEM_CONFIG['SYSTEM_REDIS_PASS'],
+    'password' => $SYSTEM_CONFIG['SYSTEM_REDIS_PASS'],
     'database' => $SYSTEM_CONFIG['SYSTEM_REDIS_DATABASE']
 ];
