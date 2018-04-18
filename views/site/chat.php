79f82afc56a815de766f39d8a054b8e8626beb23
maliang <maliang@xywy.com>
2018-04-18 12:59:17 +0800

    all

--- views/site/chat.php
+++ views/site/chat.php
@@ -45,9 +45,11 @@ AppAsset::register($this);
 <?php
 $js = <<<JS
     $(function() {
+        var ws = null;
         function connectWs() {
-            var ws = new WebSocket('ws:service.malyan.cn');
+            ws = new WebSocket('ws:service.malyan.cn');
             ws.onopen = function(e){
+                heartCheck.reset().start();
             };
             
             ws.onmessage = function(e){
@@ -63,7 +65,7 @@ $js = <<<JS
             };
             
             ws.onclose = function(e){
-                connectWs();
+                reconnect();
             };
             
             send = function() {
@@ -77,7 +79,38 @@ $js = <<<JS
                     $('.message').val('');
                 }
             }
+            
+            //心跳检测
+            var heartCheck = {
+                timeout: 540000,        //9分钟发一次心跳
+                timeoutObj: null,
+                serverTimeoutObj: null,
+                reset: function(){
+                    clearTimeout(this.timeoutObj);
+                    clearTimeout(this.serverTimeoutObj);
+                    return this;
+                },
+                start: function(){
+                    var self = this;
+                    this.timeoutObj = setTimeout(function(){
+                        //这里发送一个心跳，后端收到后，返回一个心跳消息，
+                        //onmessage拿到返回的心跳就说明连接正常
+                        ws.send("ping");
+                        console.log("ping!")
+                        self.serverTimeoutObj = setTimeout(function(){//如果超过一定时间还没重置，说明后端主动断开了
+                            ws.close();     //如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
+                        }, self.timeout)
+                    }, this.timeout)
+                }
+            }
         }
+        
+        reconnect = function(){
+            setTimeout(function(){
+                 connectWs();
+            },5000);
+        };
+        
         connectWs();
     });
 JS;
