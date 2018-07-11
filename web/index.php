d741d7c70b42ec597bb84c882761b5d05d0d8b41
maliang <maliang@xywy.com>
2018-07-11 09:34:43 +0800

    ml

--- web/index.php
+++ web/index.php
@@ -1,13 +1,17 @@
 <?php
 
 // comment out the following two lines when deployed to production
-$SYSTEM_CONFIG = parse_ini_file(__DIR__ . '/../../systems/SYSTEM_CONFIG') ;
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+$SYSTEM_CONFIG = parse_ini_file(__DIR__.'/../../systems/SYSTEM_CONFIG') ;
+if(isset($SYSTEM_CONFIG['YII_DEBUG']) &&  $SYSTEM_CONFIG['YII_DEBUG']){
+    define('YII_DEBUG', true);
+}
+if(isset($SYSTEM_CONFIG['YII_ENV']) &&  $SYSTEM_CONFIG['YII_ENV']){
+    define('YII_ENV', 'dev');
+}
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require __DIR__ . '/../config/web.php';
+$config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
