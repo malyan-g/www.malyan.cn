198e9156f035646555cbebe1fe24e7646d9952ba
maliang <maliang@xywy.com>
2018-08-10 15:34:40 +0800

    ml

---
+++ views/site/home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<video src="movie.ogg" controls="controls" autoplay="autoplay" loop="loop" src="http://img.malyan.cn/AgyF3kih_1677643147_hd.mp4" width="100%">
+    您的浏览器不支持 video 标签。
+</video>
+</body>
+</html>
